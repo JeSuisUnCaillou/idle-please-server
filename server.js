2b9396b078fce09ac0d5ac30366f91a32eb38662
Caillou <igorlacroute@gmail.com>
2018-05-28 21:40:31 +0200

    remove controller code from server.js

--- server.js
+++ server.js
@@ -1,11 +1,8 @@
+'use strict'
 var express = require('express')
 
 var app = express()
 var port = process.env.PORT || 8081
-app.get('/', function(req, res) {
-    res.setHeader('Content-Type', 'text/plain')
-    res.send('Vous êtes à l\'accueil')
-})
 
 app.listen(port)
 console.log('server started on port '+ port)
